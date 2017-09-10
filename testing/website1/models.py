cf4d6206fa2abba42127cd3cb70ba01e1f886368
Sean Dvir <zazi.dx@gmail.com>
2017-09-11 00:04:29 +0300

    SlugField & the Unique Slug Generator +  Signal for Unique Slugs
    making slug field, doing pre save and post save with the instance of object being created.

--- testing/website1/models.py
+++ testing/website1/models.py
@@ -1,13 +1,35 @@
-import datetime
-
 from django.db import models
-from django.utils import timezone
+from django.db.models.signals import pre_save, post_save
 
-# Create your models here.
+from .utils import unique_slug_generator
+
 class Weight(models.Model):
 	name = models.CharField(max_length = 120, null = True)
 	gym = models.BooleanField(default = False)
-	date = models.DateTimeField(auto_now = False, auto_now_add = False, null = True)
+	timestamp = models.DateTimeField(auto_now = False, auto_now_add = False, null = True)
 	weight = models.FloatField(max_length = 5, null = True)
+	slug = models.SlugField(blank = True, null = True)
+
 	def __str__(self):
 		return self.name
+
+	@property
+	def title(self):
+		return self.name
+
+def rl_pre_save_reciever(sender, instance, *args, **kwargs):
+	print('saving...')
+	print(instance.timestamp)
+	if not instance.slug: 
+		instance.slug = unique_slug_generator(instance)
+
+def rl_post_save_reciever(sender, instance, created, *args, **kwargs):
+	print('saved')
+	print(instance.timestamp)
+	if not instance.slug: 
+		instance.slug = unique_slug_generator(instance)
+		instance.save()
+
+pre_save.connect(rl_pre_save_reciever, sender = Weight)
+
+post_save.connect(rl_post_save_reciever, sender = Weight)
