a41f71550dcae22f6d09e4086d839047e8e83f75
Sean Dvir <zazi.dx@gmail.com>
2017-09-14 00:53:59 +0300

    Add files via upload

--- testing/testing/settings.py
+++ testing/testing/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'testing.urls'
+LOGIN_URL = '/login/'
 
 TEMPLATES = [
     {
--- testing/testing/urls.py
+++ testing/testing/urls.py
@@ -1,11 +1,16 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 
 from django.views.generic import TemplateView
+
+from django.contrib.auth.views import LoginView
+
 from website1.views import Weights, WeightListView, WeightDetailView, WeightCreateView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', LoginView.as_view(), name = 'login'),
     url(r'^weight/$', WeightListView.as_view()),
     url(r'^weight/create/$', WeightCreateView.as_view()),
     url(r'^weight/(?P<slug>[\w-]+)/$', WeightDetailView.as_view()), #view with the get_object function
